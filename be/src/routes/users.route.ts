038512f2c58bcde6a31085895d8360355e672b22
toan_nguyen <nguyenduytoan@bkdn@gmail.com>
2026-08-18 18:56:21 +0700

    hanle error

--- be/src/routes/users.route.ts
+++ be/src/routes/users.route.ts
@@ -1,11 +1,43 @@
-import { Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 import { loginController, registerController } from '~/controllers/user.controller'
 import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 import { validate } from '~/utils/validation'
 
 const usersRouter = Router()
 
-usersRouter.post('/login', loginValidator, loginController)
+// usersRouter.post('/login', loginValidator, loginController)
+usersRouter.post(
+    '/login',
+    loginValidator,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            console.log('1')
+            // next(new Error(' loi roi ban oi')) 
+            throw new Error("loi roi ban oi 1")
+        } catch (error) {
+            next(error)
+        }
+
+    },
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log('2')
+        next()
+    },
+
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log('3')
+        return res.json({
+            message: 'ok'
+        })
+    },
+    (err: Error, req: Request, res: Response, next: NextFunction) => {
+        console.log('error', err.message)
+        return res.status(400).json({
+            message: err.message
+        })
+    }
+    // loginController
+)
 
 /**
  * Description: Register new user
